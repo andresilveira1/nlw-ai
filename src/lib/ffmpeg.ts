f062fb6cdb7cf834a87fa30f264c036162dcbbba
André Silveira <andre.silveira1silva@gmail.com>
2023-09-18 23:14:24 -0300

    feat: fix import path

--- src/lib/ffmpeg.ts
+++ src/lib/ffmpeg.ts
@@ -1,6 +1,6 @@
 import { FFmpeg } from '@ffmpeg/ffmpeg'
 import coreURL from '../ffmpeg/ffmpeg-core.js?url'
-import wasmURL from '../ffmpeg/ffmpeg-wasm?url'
+import wasmURL from '../ffmpeg/ffmpeg-core.wasm?url'
 import workerURL from '../ffmpeg/ffmpeg-worker.js?url'
 
 let ffmpeg: FFmpeg | null
